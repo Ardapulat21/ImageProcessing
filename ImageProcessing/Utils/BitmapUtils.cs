53ebe0d061e00c428239d6dec775da69b1c3bf60
Arda <ardapulat21@outlook.com>
2024-07-02 08:14:33 +0300

    Update BitmapUtils.cs

--- ImageProcessing/Utils/BitmapUtils.cs
+++ ImageProcessing/Utils/BitmapUtils.cs
@@ -17,30 +17,6 @@ namespace ImageProcessing
 {
     public static class BitmapUtils
     {
-        [System.Runtime.InteropServices.DllImport("gdi32.dll")]
-        public static extern bool DeleteObject(IntPtr hObject);
-
-        public static BitmapImage BitmapToBitmapImage(Bitmap bitmap)
-        {
-            IntPtr hBitmap = bitmap.GetHbitmap();
-            BitmapImage retval;
-
-            try
-            {
-                retval = (BitmapImage)Imaging.CreateBitmapSourceFromHBitmap(
-                             hBitmap,
-                             IntPtr.Zero,
-                             Int32Rect.Empty,
-                             BitmapSizeOptions.FromEmptyOptions());
-            }
-            finally
-            {
-                DeleteObject(hBitmap);
-            }
-
-            return retval;
-        }
-
         public static unsafe Bitmap ToBitmap(this ImageData bitmap)
         {
             fixed (byte* p = bitmap.Data)
@@ -66,22 +42,6 @@ namespace ImageProcessing
             }
 
             return bitmapImage;
-        }
-        public static BitmapImage ToBitmapImage(this Bitmap bitmap)
-        {
-            using (var memory = new MemoryStream())
-            {
-                bitmap.Save(memory, ImageFormat.Bmp);
-                memory.Position = 0;
-
-                var bitmapImage = new BitmapImage();
-                bitmapImage.BeginInit();
-                bitmapImage.StreamSource = memory;
-                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
-                bitmapImage.EndInit();
-                bitmapImage.Freeze();
-                return bitmapImage;
-            }
         }
     }
 }
