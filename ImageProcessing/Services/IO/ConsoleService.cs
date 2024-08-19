51990df474f0ab1a710fa92933a85afbef27a28a
Arda <ardapulat21@outlook.com>
2024-08-19 17:38:09 +0300

    Console service added

---
+++ ImageProcessing/Services/IO/ConsoleService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageProcessing.Services.IO
+{
+    public enum Color
+    {
+        Green,
+        Yellow,
+        Red,
+    }
+    public class ConsoleService
+    {
+        public static void WriteLine(string text,Color color)
+        {
+            switch (color)
+            {
+                case Color.Red:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(text);
+                    break;
+                case Color.Green:
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(text);
+                    break;
+                case Color.Yellow:
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine(text);
+                    break;
+            }
+        }
+    }
+}
