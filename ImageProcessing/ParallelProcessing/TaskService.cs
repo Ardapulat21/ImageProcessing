345d03fb2ecf3d5a16783d2e1b22222b56b770ee
Arda <ardapulat21@outlook.com>
2024-09-24 13:33:38 +0300

    Task service has been added

---
+++ ImageProcessing/ParallelProcessing/TaskService.cs
@@ -0,0 +1,43 @@
+﻿using ImageProcessing.Services.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ImageProcessing.ParallelProcessing
+{
+    public abstract class TaskService
+    {
+        public Task Task;
+        public CancellationTokenSource CancellationTokenSource;
+        public CancellationToken CancellationToken;
+        public async Task CreateTask(Action action)
+        {
+            CancellationTokenSource = new CancellationTokenSource();
+            CancellationToken = CancellationTokenSource.Token;
+            try
+            {
+                Task = Task.Run(action, CancellationToken);
+                await Task;
+            }
+            catch (OperationCanceledException){ }
+            catch { }
+        }
+        public async Task Cancel()
+        {
+            if (Task.Status == TaskStatus.WaitingForActivation)
+            {
+                CancellationTokenSource.Cancel();
+                try
+                {
+                    await Task;
+                }
+                catch (OperationCanceledException) { }
+                catch { }
+            }
+        }
+        public abstract void Reset();
+    }
+}
