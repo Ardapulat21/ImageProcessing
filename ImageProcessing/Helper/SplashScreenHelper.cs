23c991193cafc80dbfd6991fd1b3d2a2192e5d63
Arda <ardapulat21@outlook.com>
2024-09-26 16:22:11 +0300

    Loading Screen

---
+++ ImageProcessing/Helper/SplashScreenHelper.cs
@@ -0,0 +1,24 @@
+﻿using ImageProcessing.ViewModels;
+using Microsoft.VisualBasic;
+using System;
+using SplashScreen = ImageProcessing.UserControls.SplashScreen;
+namespace ImageProcessing.Helper
+{
+    public class SplashScreenHelper
+    {
+        static SplashScreenViewModel _splashScreenViewModel = SplashScreenViewModel.GetInstance();
+        public static void Show()
+        {
+            _splashScreenViewModel.MakeVisible();
+        }
+        public static void Hide()
+        {
+            _splashScreenViewModel.MakeHidden();
+        }
+        public static void Progress(double value)
+        {
+            Show();
+            _splashScreenViewModel.SetProgress(value);
+        }
+    }
+}
---
+++ ImageProcessing/ViewModels/SplashScreenViewModel.cs
@@ -0,0 +1,81 @@
+﻿using ImageProcessing.Helper;
+using ImageProcessing.Services.IO;
+using System.ComponentModel;
+using System.Security.Principal;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace ImageProcessing.ViewModels
+{
+    public class SplashScreenViewModel : INotifyPropertyChanged
+    {
+        private Visibility _visibility = Visibility.Hidden;
+        public Visibility Visibility { 
+            get => _visibility; 
+            set
+            {
+                _visibility = value;
+                OnPropertyChanged(nameof(Visibility));
+            }
+        }
+        private string _loadingText = "Loading..";
+        public string LoadingText 
+        { 
+            get => _loadingText;
+            set
+            {
+                _loadingText = value;
+                OnPropertyChanged(nameof(LoadingText));
+            }
+        }
+        private double _progressValue = 0;
+        public double ProgressValue
+        {
+            get => _progressValue;
+            set
+            {
+                _progressValue = value;
+                OnPropertyChanged(nameof(ProgressValue));
+                LoadingText = $"Loading.. %{100 * _progressValue:n0}";
+                if (value >= 0.99)
+                    SplashScreenHelper.Hide();
+            }
+        }
+        public void MakeVisible()
+        {
+            if (Visibility == Visibility.Hidden)
+                Visibility = Visibility.Visible;
+        }
+        public void MakeHidden()
+        {
+            if (Visibility == Visibility.Visible)
+                Visibility = Visibility.Hidden;
+
+        }
+        public void SetProgress(double value)
+        {
+            Dispatcher.CurrentDispatcher.Invoke(() => ProgressValue = value);
+        }
+        #region Singleton
+        static SplashScreenViewModel _splashScreenViewModel;
+        public static SplashScreenViewModel GetInstance()
+        {
+            if (_splashScreenViewModel == null)
+                _splashScreenViewModel = new SplashScreenViewModel();
+            return _splashScreenViewModel;
+        }
+        private SplashScreenViewModel()
+        {
+
+        }
+        #endregion
+        #region MVVM
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
+    
